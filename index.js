af2472ba0778b144ef11f92b1da26f56b3c99034
Liam Slagle <liam.slagle@gmail.com>
2020-01-24 16:40:37 -0600

    Use heroku config vars

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 var Twit = require('twit');
-var credentials = require('./credentials.json');
 var r = require('./resources.json');
 
 var T = new Twit({
-  consumer_key: credentials.consumer_key,
-  consumer_secret: credentials.consumer_secret,
-  access_token: credentials.access_token,
-  access_token_secret: credentials.access_token_secret
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 
 var templates = {
@@ -52,6 +51,9 @@ var templates = {
       sample(r.punctuation) + sample(r.goodbyes) + sample(r.first_person)
   },
   corporation: function() {
+    //return smaple(r.corporate_handles) + ' ' +
+      //sample(['', sample(r.greetings) + ' ']) + 'my ' +
+      //sample(r.my_relatives) + 
   },
   recipe: function() {
   }
